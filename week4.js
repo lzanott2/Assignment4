e40682680f130e9653b8fb379b99a7252db1af57
KittyGirl <lzanott2@du.edu>
2015-02-05 01:43:56 -0700

    Update

---
+++ week4.js
@@ -0,0 +1,104 @@
+$(document).ready(function() {
+// Real-time Validation 
+    
+    //Name can't be blank
+    $('#contact_name').on('input', function() {
+        var input = $(this);
+        var is_name = input.val();
+
+        if(is_name && (is_name.length > 0)) {
+            input.removeClass("invalid").addClass("valid");
+        }
+        else {input.removeClass("valid").addClass("invalid");
+        }
+    });
+    
+
+    //Email must be valid 
+    $('#contact_email').on('input', function() {
+        var input = $(this);
+        var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        var is_email = re.test(input.val());
+
+        if(is_email) {
+            input.removeClass("invalid").addClass("valid");
+        }
+        else {
+            input.removeClass("valid").addClass("invalid");
+        }
+    });
+    
+    
+    //Address can't be blank 
+    $('.contact_address').on('input', function() {
+        var input = $(this);
+        var address = $(this).val();
+
+        if(address && (address.length > 0)) {
+            input.removeClass("invalid").addClass("valid");
+        }
+        else {
+            input.removeClass("valid").addClass("invalid");
+        }   
+    });
+
+
+    //Phone must be 10 digits 
+    $( "#contact_phone" ).on('input', function() {
+        var input = $(this);
+        var is_phone = input.val();
+
+        if(is_phone && !isNaN(is_phone) && is_phone.length === 10) {
+            input.removeClass("invalid").addClass("valid");
+            $("input").css("border", "green");
+        }
+        else {
+            input.removeClass("valid").addClass("invalid");
+            $("input").css("border", "red");
+        }
+    });
+
+
+    //Styling of the phone number.
+    $("input[@name='phone']").keyup(function() {
+        var input = this.value.length;
+        var is_phone = $(this).val();
+
+        //Can also use (input === 3) as the condition
+        if (input[2]) {
+            $("input[@name='phone']").val("(" + is_phone + ")" + " ");
+        } 
+        //Can also use (input === 9) as the condition
+        else if (input[5]) {
+            $("input[@name='phone']").val(is_phone + "-");
+        }
+    });
+
+
+    // After Form Submitted Validation
+    $("#contact_submit button").click(function(event) {
+        var form_data = $("#contact").serializeArray();
+        var error_free = true;
+
+        for (var input in form_data) {
+            var element = $("#contact_" + form_data[input]['name']);
+            var valid = element.hasClass("valid");
+            var error_element = $("span", element.parent());
+
+            if (!valid) {
+                error_element.removeClass("error").addClass("error_show"); 
+                error_free = false;
+            }
+            else {
+                error_element.removeClass("error_show").addClass("error");
+            }
+        }
+        
+        if (!error_free) {
+            event.preventDefault(); 
+        }
+        else {
+            alert('No errors: Form will be submitted');
+        }
+    });
+});
